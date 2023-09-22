aa1b3bdcec6336bf8140b943651c08bc1f5152df
HossamAhmedGH <hossam.ahmed.abdelwahab@gmail.com>
2023-09-21 20:35:51 -0700

    asdfaf

--- print_rev.c
+++ print_rev.c
@@ -1,4 +1,4 @@
-3#include <stdarg.h>
+#include <stdarg.h>
 #include <stdio.h>
 #include "main.h"
 /**
@@ -14,7 +14,7 @@ int print_rev(va_list r)
 	st = va_arg(r, char *);
 	if (st == NULL)
 		st = ")llun(";
-	for (i = 0; st[i] != '\0'; i++)
+	 for (i = 0; st[i] != '\0'; i++)
 		;
 	for (i -= 1 ; i >= 0; i--)
 	{
